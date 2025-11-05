74702e668df29f83afc2dfbdc041e9f676d92bad
CaioNasc18 <caio.nascimento.1702@gmail.com>
2025-11-05 00:31:01 +0000

    commit funcao criarCat

--- classes.cs
+++ classes.cs
@@ -6,7 +6,35 @@ public class categoria
     int id;
     string nome;
     string descricao;
-    
+
+    public void Criar()
+    {
+        Console.WriteLine("Qual será a categoria?");
+        nome = Console.ReadLine();
+
+        Console.WriteLine("Quer adicionar alguma descrição?");
+        descricao = Console.ReadLine();
+
+        if (descricao.trim == "")
+        {
+            descricao == "SEM DESCRIÇÃO";
+        }
+
+        random rnd = new random;
+        id = rnd.Next(1, 100000)
+
+        //TODO: CASO ID JA EXISTA, CRIAR OUTRO ID
+    }
+
+    public void Editar(nome, descricao)
+    {
+
+    }
+
+    public void Eliminar()
+    {
+
+    }
 }
 
 public class utilizador
