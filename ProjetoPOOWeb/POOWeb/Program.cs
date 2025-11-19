8ac2e4591353ed44b68ceaef40d30ab2b45c2611
CaioNasc18 <caio.nascimento.1702@gmail.com>
2025-11-19 15:14:06 +0000

    commit fim começo webdev

--- ProjetoPOOWeb/POOWeb/Program.cs
+++ ProjetoPOOWeb/POOWeb/Program.cs
@@ -1,8 +1,27 @@
+using HelloWorldApp;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-app.UseStaticFiles(); // permite servir HTML, CSS, JS, imagens
-app.MapGet("/", async context =>
+
+app.UseStaticFiles();
+
+// Página inicial -> index.html
+app.MapGet("/", context =>
 {
     context.Response.Redirect("index.html");
+    return Task.CompletedTask;
 });
+
+// Endpoint que devolve um objeto Product em JSON
+app.MapGet("/product", () =>
+{
+    var product = new Product
+    {
+        Id = 1,
+        Name = "Laptop",
+        Price = 1200.50m
+    };
+    return Results.Json(product);
+});
+
 app.Run();
